020fe80fb8cdf195b31e39927bd1b4921377d7b8
eternalsad <sadikcartman@gmail.com>
2020-03-18 22:41:25 +0600

    feat: create brackets check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] == bracketsConfig[j][0]) {
+        stack.push(str[i]);
+        continue;
+      }
+
+      if (stack.length == 0) {
+        return false;
+        if (stack[length - 1] == bracketsConfig[j][0]) {
+          stack.pop();
+          continue;
+        } else return false;
+      }
+    }
+  }
+  return stack.length != 0 ? false : true;
 }
