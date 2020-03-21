7e9238c0a9f5b200fd561179f76187da20bffd2b
eternalsad <sadikcartman@gmail.com>
2020-03-21 17:23:45 +0600

    fix: implement check for equal brackets

--- src/index.js
+++ src/index.js
@@ -4,18 +4,44 @@ module.exports = function check(str, bracketsConfig) {
   for (let i = 0; i < str.length; i++) {
     for (let j = 0; j < bracketsConfig.length; j++) {
       if (str[i] == bracketsConfig[j][0]) {
+        if (str[i] == bracketsConfig[j][1]) {
+          if (stack.length > 0 && stack[stack.length - 1] == str[i]) {
+            stack.pop();
+            break;
+          }
+        }
         stack.push(str[i]);
-        continue;
       }
-
-      if (stack.length == 0) {
-        return false;
-        if (stack[length - 1] == bracketsConfig[j][0]) {
-          stack.pop();
-          continue;
+      else if (str[i] == bracketsConfig[j][1] && bracketsConfig[j][1] != bracketsConfig[j][0]) {
+        if (stack.length > 0) {
+          if (stack[stack.length - 1] == bracketsConfig[j][0]) stack.pop();
+          else return false;
         } else return false;
       }
     }
   }
-  return stack.length != 0 ? false : true;
+  return stack.length == 0 ? true : false;
 }
+/*
+module.exports = function check(str, bracketsConfig) {
+  // your solution
+  let stack = [];
+  let ans;
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      //if (bracketsConfig[j][0] != bracketsConfig[j][1]) {
+      if (str[i] == bracketsConfig[j][0] && bracketsConfig[j][0] != bracketsConfig[j][1]) {
+        stack.push(str[i]);
+        //break;
+      }
+      if (stack.length != 0) {
+        if (str[i] == bracketsConfig[j][1] && stack[stack.length - 1] == bracketsConfig[j][0]) {
+          stack.pop();
+          break;
+        }
+      } else return false;
+      //stack.push(str[i]);
+    }
+  }
+  return (stack.length == 0) ? true : false;
+}*/
